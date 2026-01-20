bfa1d57e48e9cd0c488bcaa0099843f43ce0c624
Vincent Dalstra <vincent.dalstra@outlook.com>
2026-01-20 16:07:50 +0800

    Simple text I/O example

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,31 @@ const std = @import("std");
 const zusbview = @import("zusbview");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try zusbview.bufferedPrint();
+    // Memory
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var aalloc = arena.allocator();
+
+    const stdin_buf: []u8 = try aalloc.alloc(u8, (16 * 1024));
+    var stdin_reader = std.fs.File.stdin().reader(stdin_buf);
+    const stdin: *std.io.Reader = &stdin_reader.interface;
+
+    const stdout_buf: []u8 = try aalloc.alloc(u8, (16 * 1024));
+    var stdout_writer = std.fs.File.stdout().writer(stdout_buf);
+    const stdout: *std.io.Writer = &stdout_writer.interface;
+
+    try stdout.writeAll("HELLO!\n");
+    try stdout.flush();
+
+    var line_no: usize = 0;
+    while (true) {
+        const bare_line = try stdin.takeDelimiter('\n') orelse break;
+        const line = std.mem.trim(u8, bare_line, "\r");
+        line_no += 1;
+
+        try stdout.print("line {}, length {}: {s}\n", .{ line_no, line.len, line });
+        try stdout.flush();
+    }
 }
 
 test "simple test" {
